37ea18c40dd95c66d731bc080bf791d3b85eade6
Monayam-Hossain <hmonayam@gmail.com>
2026-09-11 23:06:31 +0600

    Project name DevStack

---
+++ DevStack/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <>
+    <h2 className="text-5xl text-red-500">Hello React</h2>
+    <button className="btn btn-error">Error</button>
+    </>
+  )
+}
+
+export default App
